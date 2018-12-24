69a85f92cfd8f5835bedb7ae1e3717e7f449ca1d
Alon <alon.ttp@gmail.com>
2018-12-24 20:33:07 +0200

    Thread II
    Executor

--- Thread/app/src/main/java/com/example/alon/thread/MainActivity.java
+++ Thread/app/src/main/java/com/example/alon/thread/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.alon.thread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "Thread";
+    private Executor mainThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,30 @@ public class MainActivity extends AppCompatActivity {
         CustomThread thread = new CustomThread();
         thread.start();
 
-        // Create a thread and start it running
-        Thread runnable = new Thread(new CustomRunnable());
-        runnable.start();
+        /*
+            The Runnable interface should be implemented by any class whose instances are intended to be executed by a thread.
+            The class must define a method of no arguments called run.
+         */
+            // Create a thread and start it running
+        Thread runnableThread = new Thread(new CustomRunnable());
+        runnableThread.start();
 
-        Log.d(TAG, "MainActivity");
+        Executor executor = new anExecutor(); // Object that executes submitted Runnable tasks
+        executor.execute(new CustomRunnable());
+        executor.execute(new CustomRunnable2());
+
+        // Creates a thread pool that creates new threads as needed, but will reuse previously constructed threads when they are available.
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        Log.d(TAG, String.valueOf(threadPoolExecutor.getPoolSize()));
+        threadPoolExecutor.execute(new CustomRunnable());
+        Log.d(TAG, String.valueOf(threadPoolExecutor.getPoolSize()));
+
+    }
+
+    class anExecutor implements Executor {
+        public void execute(Runnable task) {
+            task.run();
+        }
     }
 
     public class CustomThread extends Thread {
@@ -41,6 +64,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             Log.d(TAG, "CustomRunnable");
+        }
+    }
+
+    public class CustomRunnable2 implements Runnable {
+        @Override
+        public void run() {
+            Log.d(TAG, "CustomRunnable2");
         }
     }
 }
